b3f1e31c395dfe56c6b085608bb1b79c977f7254
Mahesh <maheshdsu86@gmail.com>
2023-05-26 22:41:19 +0530

    signinpage

--- frontend/src/Components/Signin/Signin.js
+++ frontend/src/Components/Signin/Signin.js
@@ -1,11 +1,43 @@
 import React from 'react'
 import './Signin.css'
+import {Link} from 'react-router-dom'
+import logo from '../Assets/logos/logo_white.avif'
+
+
 
 const Signin = () => {
   return (
-    <div>
-      <h1>signin</h1>
-    </div>
+    <>
+    <div className="bgi">
+          <div className="div2">
+              <img alt='' src={logo}/>
+              <div className="signInContainer">
+                  <p>Sign In</p>
+                  <input className='signInputs' type='text' placeholder='username '/>
+                  <br/>
+                  <input className='signInputs' type='password' placeholder='password'/>
+                  <br/>
+                  <ul>
+                      <li className='fac'><i class="fa-brands fa-facebook"></i></li>
+                      <li className='goo'><i class="fa-brands fa-google"></i></li>
+                      <li className='app'><i class="fa-brands fa-apple"></i></li>
+                      <li className='xbo'><i class="fa-brands fa-xbox"></i></li>
+                  </ul>
+                  <input className='cbox' type="checkbox"/>
+                  <label className='cboxLabel'>Stay signed in</label>
+                  <br/>   
+                  <button className='SignBtnn'>sign in</button>
+                  <br/>  
+                  <Link className='signElinks'>can't sign in?</Link>
+                  <br/>  
+                  <Link className='signElinks' to={'signup'}>create account</Link>
+
+                  
+              </div>
+            
+          </div>
+      </div>
+  </>
   )
 }
 
--- frontend/src/Components/Signup/Signup.js
+++ frontend/src/Components/Signup/Signup.js
@@ -1,11 +1,45 @@
 import React from 'react'
+// import {Form, Link} from 'react-router-dom'
 import './Signup.css'
+import logo from '../Assets/logos/logo_white.avif'
+
 
 const Signup = () => {
   return (
-    <div>
-      <h1>signup</h1>
-    </div>
+    <>
+     <div className="signUpBgi">
+          <div className="div2">
+              <img alt='' src={logo}/>
+              <div className="signInContainer">
+                  <p>Sign up</p>
+                  {/* <input className='signInputs' type='text' placeholder='username '/>
+                  <br/>
+                  <input className='signInputs' type='password' placeholder='password'/>
+                  <br/>
+                  <ul>
+                      <li className='fac'><i class="fa-brands fa-facebook"></i></li>
+                      <li className='goo'><i class="fa-brands fa-google"></i></li>
+                      <li className='app'><i class="fa-brands fa-apple"></i></li>
+                      <li className='xbo'><i class="fa-brands fa-xbox"></i></li>
+                  </ul>
+                  <input className='cbox' type="checkbox"/>
+                  <label className='cboxLabel'>Stay signed in</label>
+                  <br/>   
+                  <button className='SignBtnn'>sign in</button>
+                  <br/>  
+                  <Link className='signElinks'>can't sign in?</Link>
+                  <br/>  
+                  <Link className='signElinks'>create account</Link> */}
+                  <form>
+
+                  </form>
+
+                  
+              </div>
+            
+          </div>
+      </div>
+    </>
   )
 }
 
