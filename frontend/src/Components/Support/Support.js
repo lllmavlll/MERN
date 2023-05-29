ca98f541ed3f16d63932b11339ab61c8c56c78e6
Mahesh <maheshdsu86@gmail.com>
2023-05-29 23:24:18 +0530

    support page

--- frontend/src/Components/Support/Support.js
+++ frontend/src/Components/Support/Support.js
@@ -8,18 +8,18 @@ const Support =()=>{
    // }
    return (
        <div className="supMain">
+        <form action='/support' method='post'>
            <div className="supContainer">
                <p className='supHead'>how can we help you?</p>
                <div className="supBanner">
-                   <img alt=''  src={supBanner}/>
+                  <img alt=''  src={supBanner}/>
                </div>
-               <input autoComplete='off'  placeholder='username'name='username' className="supName" type='text'/>
-               <input autoComplete='off'  placeholder='email' name='email' className="supEmail" type='email'/>
-               <textarea autoComplete='off'  placeholder='enter your message' name='issue' className="supMessage" type='text'/>
-               <button   className="supSubBtn" >submit</button>
-
+                  <input autoComplete='off'  placeholder='username'name='username' className="supName" type='text'/>
+                  <input autoComplete='off'  placeholder='email' name='email' className="supEmail" type='email'/>
+                  <textarea autoComplete='off'  placeholder='enter your message' name='issue' className="supMessage" type='text'/>
+                  <button type='submit'   className="supSubBtn" >submit</button>
            </div>
-
+        </form>
        </div>
    )
 
