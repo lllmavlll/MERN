cfd64b95c20fefacb52bcd9a8981dbeadd0ffb74
Mahesh <maheshdsu86@gmail.com>
2023-05-30 21:35:04 +0530

    signup data to mongo db

--- frontend/src/Components/Signin/Signin1.js
+++ frontend/src/Components/Signin/Signin1.js
@@ -14,16 +14,16 @@ const Signin = () => {
               <div className="signInContainer">
                 <form action='/signin' method='post'>
                   <p className='SHeader'>Sign In</p>
-                  <input required className='signInputs' autoComplete='off' name='username' type='text' placeholder='username '/>
+                  <input required className='signInputs' autoComplete='off' name='email' type='email' placeholder='email'/>
                   <br/>
                   <input required className='signInputs' autoComplete='off' name='password' type='password' placeholder='password'/>
                   <br/>
-                  <ul>
+                  {/* <ul>
                       <li className='fac'><i class="fa-brands fa-facebook"></i></li>
                       <li className='goo'><i class="fa-brands fa-google"></i></li>
                       <li className='app'><i class="fa-brands fa-apple"></i></li>
                       <li className='xbo'><i class="fa-brands fa-xbox"></i></li>
-                  </ul>
+                  </ul> */}
                   <input className='cbox' type="checkbox"/>
                   <label className='cboxLabel'>Stay signed in</label>
                   <br/>   
--- frontend/src/Components/Signup/Signup.js
+++ frontend/src/Components/Signup/Signup.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import {Form, Link} from 'react-router-dom'
 import './Signup.css'
 import logo from '../Assets/logos/logo_white.avif'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate,   } from 'react-router-dom'
 
 
 const Signup = () => {
+  const navigate = useNavigate()
+  const [user, setUser]=useState({
+      username:"",
+      email:"",
+      password:"",
+      cpassword:""
+  })
+
+  let name, value;
+  const userHandler=(e)=>{
+    console.log(user);
+    name = e.target.name;
+    value = e.target.value;
+    
+    setUser({...user, [name]:value})
+
+  }
+
+  const saveData = async(e)=>{
+    e.preventDefault();
+
+    const {username, email, password, cpassword} = user
+
+    const res =await fetch('/signup',{
+      method:'POST',
+      headers:{
+        "content-type":"application/json"
+      },
+      body:JSON.stringify({
+        username, email, password, cpassword
+      })
+    })
+
+    const data = await res.json();
+    if(res.status=== 422||!data){
+      window.alert("signup failed")
+      console.log("signup failed")
+    }else{
+      window.alert("signup success!")
+      console.log("signup success!")
+    }
+    navigate('/signin')
+
+  }
+
   return (
     <>
      <div className="signUpBgi">
@@ -13,34 +58,48 @@ const Signup = () => {
               <img alt='' src={logo}/>
               <div className="signInContainer">
                   <p className='SHeader'>Sign up</p>
-                  {/* <input className='signInputs' type='text' placeholder='username '/>
-                  <br/>
-                  <input className='signInputs' type='password' placeholder='password'/>
-                  <br/>
-                  <ul>
-                      <li className='fac'><i class="fa-brands fa-facebook"></i></li>
-                      <li className='goo'><i class="fa-brands fa-google"></i></li>
-                      <li className='app'><i class="fa-brands fa-apple"></i></li>
-                      <li className='xbo'><i class="fa-brands fa-xbox"></i></li>
-                  </ul>
-                  <input className='cbox' type="checkbox"/>
-                  <label className='cboxLabel'>Stay signed in</label>
-                  <br/>   
-                  <button className='SignBtnn'>sign in</button>
-                  <br/>  
-                  <Link className='signElinks'>can't sign in?</Link>
-                  <br/>  
-                  <Link className='signElinks'>create account</Link> */}
-                  <form action='/signup' method='post'>
-                    <input required className='signInputs' autoComplete='off' name='username' type='text' placeholder='username '/>
+                  <form  method='POST'>
+                    <input 
+                    required 
+                    className='signInputs' 
+                    autoComplete='off' 
+                    value={user.name} 
+                    name='username' 
+                    type='text' 
+                    onChange={userHandler}
+                    placeholder='username '/>
                     <br/>
-                    <input required className='signInputs' autoComplete='off' name='email' type='email' placeholder='email'/>
+                    <input 
+                    required 
+                    className='signInputs' 
+                    autoComplete='off' 
+                    value={user.email} 
+                    name='email' 
+                    type='email'
+                    onChange={userHandler} 
+                    placeholder='email'/>
                     <br/>
-                    <input required className='signInputs' autoComplete='off' name='password' type='password' placeholder='password '/>
+                    <input 
+                    required 
+                    className='signInputs' 
+                    autoComplete='off' 
+                    value={user.password} 
+                    name='password' 
+                    type='password'
+                    onChange={userHandler}
+                    placeholder='password '/>
                     <br/>
-                    <input required className='signInputs' autoComplete='off' name='cpassword' type='password' placeholder='confirm password'/>
+                    <input 
+                    required 
+                    className='signInputs' 
+                    autoComplete='off' 
+                    value={user.cpassword} 
+                    name='cpassword' 
+                    type='password'
+                    onChange={userHandler} 
+                    placeholder='confirm password'/>
                     <br/>
-                    <input type='submit' className='SignBtnn' value='sign up'/>
+                    <input type='submit' onClick={saveData} className='SignBtnn' value='sign up'/>
                     <br/>
                  <p className='newUserlink'> already have an account  <NavLink className='signElinks' to='/signin '> Login?</NavLink></p> 
                  </form>
