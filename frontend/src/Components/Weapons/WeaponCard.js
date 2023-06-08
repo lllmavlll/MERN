5cedff05f11b511eb54bf6ada6cf490ff1afb33c
Mahesh <maheshdsu86@gmail.com>
2023-06-08 20:51:49 +0530

    some bug fixed in css part

---
+++ frontend/src/Components/Weapons/WeaponCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const WeaponCard = (props) => {
+  return (
+    <>
+        <div className='wep1'>
+              <img src={props.src} alt=''/>
+        </div>
+    </>
+  )
+}
+
+export default WeaponCard
--- frontend/src/Components/Weapons/Weapons.js
+++ frontend/src/Components/Weapons/Weapons.js
@@ -1,5 +1,9 @@
 import React, {} from 'react'
 import './Weapons.css'
+import Img1 from '../Assets/weapons/wep.png'
+import WeaponCard from './WeaponCard'
+import shef from '../Assets/weapons/sherif.png'
+
 
 
 const Weapons = () => {
@@ -8,10 +12,28 @@ const Weapons = () => {
   return (
     <>
     <div className='wepMain'>
-      <div  style={{minHeight:'85vh'}} className='wepBG'>
-        <h1>Weapons</h1>
-      
-      </div>
+      <h1 className='sec1h'><b>Weapons</b></h1>
+        <section style={{minHeight:'85vh'}} className='wepBG'>
+          <div className='wepG1'>
+            <p>In Valorant, there are a plethora of weapons to learn and keep track of. Here’s a breakdown of all 17 weapons in Valorant — from when to use them and what their strengths and weaknesses are.</p>
+            <p>Agents have different variations of weapons, typically firearms, to cater to specific playstyles or metas. Weapons are separated by two categories, Sidearms and Primary weapons. Sidearms consists of pistols, whilst primaries include SMGs, shotguns, rifles, sniper rifles, and machine guns.</p>
+            <p>Guns have limited amounts of ammo but come with reserve magazines. Bullets deal different amounts of damage depending on the gun, the body part they hit, and the distance between the shooter and the target. Each gun deals a set amount of damage to an enemy's body at its shortest range and then uses multipliers on this value to calculate the damage it will deal to the head and legs at close range and all body parts at further ranges. Resulting numbers with a decimal value are rounded down to the next integer value. Enemies hit by bullets will also be tagged with a 72.5% slow for a brief period. Shotgun pellets will only apply a 30% slow if they tag an enemy beyond 10 meters.</p>
+            <p>Weapons can be purchased by Agents using creds during the pre-round sequence. Each Agent is equipped with a Knife and the Classic sidearm for free by default. Agents will keep the weapons they purchase through each round until they die. Weapons can also be picked up off the ground from deceased players, both friendly and hostile, which will carry on with the player into the next round.</p>
+          </div>
+          <div className='wepG2'>
+            <img src={Img1} alt=''/>
+          </div>
+        </section>
+
+        <sectiton className='wepBG2'>
+          <div className='sec2grid'>
+            <WeaponCard src={shef}/>
+            <WeaponCard src={shef}/>
+            <WeaponCard src={shef}/>
+            <WeaponCard src={shef}/>
+          </div>
+
+        </sectiton>
     </div>
     </>
   )
